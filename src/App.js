d80f32aa5517446e1e4e76fae3ddc1da745b955c
dinarsta <rositadina816@gmail.com>
2024-04-02 16:06:10 +0700

    Random-Quotes-Generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-    return ( <
-        div div className = "App" >
-        halo dina!
+    const [randomQuote, setRandomQuote] = useState('');
 
+    useEffect(() => {
+        fetchRandomQuote();
+    }, []);
+
+    const fetchRandomQuote = async() => {
+        try {
+            const response = await fetch('https://api.quotable.io/random');
+            const data = await response.json();
+            setRandomQuote(`${data.content} - ${data.author}`);
+        } catch (error) {
+            console.error('Error fetching random quote:', error);
+        }
+    };
+
+    const generateRandomQuote = () => {
+        fetchRandomQuote();
+    };
+
+    return ( <
+        div className = "App" >
         <
+        header className = "App-header" >
+        <
+        h1 > Random Quotes Generator < /h1> <
+        div className = "quote-container" > {
+            randomQuote && ( <
+                blockquote >
+                <
+                p > { randomQuote } < /p> <
+                /blockquote>
+            )
+        } <
+        button onClick = { generateRandomQuote } > Generate Random Quote < /button> <
+        /div> <
+        /header> <
         /div>
     );
 }
